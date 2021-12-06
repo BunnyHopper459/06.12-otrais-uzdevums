8b27ab12145d582be7660c2fb86c614e3f5eaa05
Arturs Garnovs <91870789+BunnyHopper459@users.noreply.github.com>
2021-12-06 08:58:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def aprAtlaidi(summa):
+  rez=""
+  if summa<100:
+    rez="Atlaide nav piešķirts, jānomaksā vēl "+str(summa)
+  elif summa>=100 and summa <200:
+    rez ="Atlaide 5%, jānomaksā vēl " + str(summa*0.95)
+  else:
+    rez="Atlaide 10%, jānomaksā vēl " + str(summa*0.90)
+  return rez
+
+summa=float(input("Ievadiet summu: "))
+print(aprAtlaidi(summa))
